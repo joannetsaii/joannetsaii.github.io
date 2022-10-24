ab558a333567a76d8af82831ad73f2ba32616e98
joannetsaii <chihant@andrew.cmu.edu>
2022-10-24 01:47:32 -0400

    Changed overall style and added GPBO project page

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 Currently in progress. Thank you for your patience!
 
 ### Features in Progress
-- responsiveness
 - individual project pages
--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-$(document).ready(function () {
-
-
+// $(document).ready(function () {
+jQuery(function() {
     let $btns = $('.project-area .button-group button');
-
 
     $btns.click(function (e) {
 
@@ -17,56 +15,4 @@ $(document).ready(function () {
         return false;
     })
 
-    $('.project-area .button-group #btn1').trigger('click');
-
-    $('.project-area .grid .test-popup-link').magnificPopup({
-        type: 'image',
-        gallery: { enabled: true }
-    });
-
-
-    // Owl-carousel
-
-    $('.site-main .about-area .owl-carousel').owlCarousel({
-        loop: true,
-        autoplay: true,
-        dots: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            560: {
-                items: 2
-            }
-        }
-    })
-
-    // sticky navigation menu
-
-    let nav_offset_top = $('.header_area').height() + 50;
-
-    function navbarFixed() {
-        if ($('.header_area').length) {
-            $(window).scroll(function () {
-                let scroll = $(window).scrollTop();
-                if (scroll >= nav_offset_top) {
-                    $('.header_area .main-menu').addClass('navbar_fixed');
-                } else {
-                    $('.header_area .main-menu').removeClass('navbar_fixed');
-                }
-            })
-        }
-    }
-
-    navbarFixed();
-
-
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
 });
